aa61b915c6bf2f597cf6fac76a7f91ee5a78cf42
grbalmeida <g.r.almeida@live.com>
2023-03-22 22:15:50 -0300

    Migrando o Time

---
+++ organo/src/componentes/Time/index.tsx
@@ -0,0 +1,31 @@
+import { IColaborador } from '../../shared/interfaces/IColaborador';
+import Colaborador from '../Colaborador';
+import './Time.css';
+
+interface TimeProps {
+    corPrimaria: string
+    corSecundaria: string
+    nome: string
+    colaboradores: IColaborador[]
+}
+
+const Time = ({corPrimaria, corSecundaria, nome, colaboradores} : TimeProps) => {
+    return (
+        colaboradores.length > 0 && <section className="time" style={{backgroundColor: corSecundaria}}>
+            <h3 style={{borderColor: corPrimaria}}>{nome}</h3>
+            <div className="colaboradores">
+                {colaboradores.map(colaborador => {
+                    return <Colaborador
+                        corDeFundo={corPrimaria}
+                        key={colaborador.nome}
+                        nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem}
+                    />
+                })}
+            </div>
+        </section>
+    )
+};
+
+export default Time;
